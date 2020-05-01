f6307f057c1fc5fd1416de26dbc21ffe4dae318d
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-05-01 23:45:57 +0900

    feat: setRepositoryInstallationTokenの追加

--- functions/src/GithubApiClient.ts
+++ functions/src/GithubApiClient.ts
@@ -3,16 +3,44 @@ import axios from "axios";
 const BaseUrl = "https://api.github.com";
 
 export class GithubApiClient {
-  installationToken?: string;
+  appToken?: string;
+  repositoryInstallationToken?: string;
+
+  async setRepositoryInstallationToken(owner: string, repo: string) {
+    const headers = this.getAppHeader();
+    const url = `${BaseUrl}/repos/${owner}/${repo}/installation`;
+    const {
+      data: { access_tokens_url },
+    } = await axios.get(url, { headers });
+
+    const {
+      data: { token },
+    } = await axios.post(access_tokens_url, { headers });
+
+    this.repositoryInstallationToken = token;
+  }
 
   async getCommit(owner: string, repo: string, commitId: string) {
     const url = `${BaseUrl}/repos/${owner}/${repo}/commits/${commitId}`;
     return await axios.get(url, { headers: this.getInstallationHeader() });
   }
 
-  private getInstallationHeader() {
+  private getAppHeader() {
+    if (!this.appToken) {
+      throw Error("appToken required");
+    }
     return {
-      Authorization: `Bearer ${this.installationToken}`,
+      Authorization: `Bearer ${this.appToken}`,
+      Accept: "application/vnd.github.machine-man-preview+json",
+    };
+  }
+
+  private getInstallationHeader() {
+    if (!this.repositoryInstallationToken) {
+      throw Error("repositoryInstallationToken required");
+    }
+    return {
+      Authorization: `Bearer ${this.repositoryInstallationToken}`,
       Accept: "application/vnd.github.machine-man-preview+json",
     };
   }
