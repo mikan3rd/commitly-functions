f3df194890b10a0ae442a7013f8b40fd1b26d11d
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-05-07 21:41:18 +0900

    fix: コミット漏れ追記

--- functions/src/firestoreCollection.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from "firebase-admin";
-
-const UserCollectionPath = "users" as const;
-const CommitsCollectionPath = "commits" as const;
-const DailyCommitsCollectionPath = "dailyCommits" as const;
-
-export const userCollection = admin.firestore().collection(UserCollectionPath);
-export const commitCollection = admin.firestore().collection(CommitsCollectionPath);
-export const dailyCommitCollection = admin.firestore().collection(DailyCommitsCollectionPath);
